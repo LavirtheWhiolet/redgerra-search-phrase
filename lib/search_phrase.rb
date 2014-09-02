2bbc9c3ba4b29eee329253d62ae35bf24d9d5ab6
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2014-09-02 19:27:07 +0600

    Add a stub for search_phrase().

---
+++ lib/search_phrase.rb
@@ -0,0 +1,14 @@
+gem 'nokogiri'
+require 'nokogiri'
+
+# 
+# searches for a phrase in pages located at specified URLs.
+# 
+# +phrase_part+ is a part of the phrase being searched for.
+# 
+# +urls+ is a collection of URLs. It must respond to <tt>urls[i]</tt> either
+# with URL or with nil (if <tt>i</tt> is out of range).
+# 
+def search_phrase(phrase_part, urls)
+  
+end
