03b5408560cb001f2ed920155cdf455891b56432
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2014-09-03 19:14:52 +0600

    ...

--- lib/search_phrase.rb
+++ lib/search_phrase.rb
@@ -4,6 +4,7 @@ require 'nokogiri'
 require 'watir'
 require 'monitor'
 require 'object/not_in'
+require 'object/not_nil'
 
 # 
 # Result of #search_phrase().
@@ -16,23 +17,56 @@ class Phrases
   
   def initialize(phrase_part, urls, browser)
     @urls = urls
-    @phrase_part = squeeze_whitespace(phrase_part)
+    @phrase_part = squeeze_and_strip_whitespace(phrase_part)
     @browser = browser
+    @next_index_in_urls = 0
+    @cached_phrases = []
+    @size = :unknown
   end
   
   def [](index)
+    mon_synchronize do
+      while index >= @cached_phrases.size
+        url = @urls[@next_index_in_urls]
+        if url.nil?
+          @size = @next_index_in_urls
+          break
+        end
+        @next_index_in_urls += 1
+        @browser.goto url
+        paragraphs =
+          to_text_blocks(Nokogiri::HTML(@browser.html)).split_by_delimiter
+        paragraphs.each do |paragraph|
+          paragraph = squeeze_and_strip_whitespace(paragraph)
+          paragraph.split(".").map { |phrase| phrase << "." }.each do |phrase|
+            @cached_phrases << phrase if phrase.include? @phrase_part
+          end
+        end
+      end
+      return @cached_phrases[index]
+    end
   end
   
   # 
   # returns either amount of these Phrases or :unknown.
   # 
   def size_u
+    mon_synchronize do
+      @size
+    end
   end
   
   # 
   # This method should be called if these Phrases will not be used anymore.
   # 
   def close()
+    mon_synchronize do
+      @urls = nil
+      @phrase_part = nil
+      @cached_phrases = nil
+      @browser.close()
+      @browser = nil
+    end
   end
   
   private
@@ -73,14 +107,19 @@ class Phrases
   
   DS = DelimitedString
   
-  def squeeze_whitespace(str)
-    str.gsub(
-      /[\u0009-\u000D\u0020\u0085\u00A0\u1680\u180E\u2000-\u200A\u2028\u2029\u202F\u205F\u3000]+/,
-      ' '
-    )
+  WHITESPACE = "[\u0009-\u000D\u0020\u0085\u00A0\u1680\u180E\u2000-\u200A\u2028\u2029\u202F\u205F\u3000]"
+  BORDERING_WHITESPACES = /^#{whitespace}+|#{whitespace}+$/
+  WHITESPACES = /#{whitespace}+/
+  
+  def squeeze_and_strip_whitespace(str)
+    str.
+      gsub(BORDERING_WHITESPACES, "").
+      gsub(WHITESPACES, " ")
   end
   
-  def to_delimited_string(element)
+  # returns DelimitedString having text blocks delimited with
+  # DelimitedString::delimiter.
+  def to_text_blocks(element)
     case element
     when Nokogiri::XML::CDATA, Nokogiri::XML::Text
       DS.delimiter.concat(DS[str]).concat(DS.delimiter)
@@ -92,7 +131,7 @@ class Phrases
         small span strike strong sub sup time tt u wbr}
       result = if need_bordering_delimiters then DS.delimiter else DS[""] end
       result = element.children.reduce(result) do |result, child|
-        result.concat(to_delimited_string(child))
+        result.concat(to_text_blocks(child))
       end
       result.concat(DS.delimiter) if need_bordering_delimiters
       return result
