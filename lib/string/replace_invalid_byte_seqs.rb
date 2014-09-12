928827ca286e8c4d26b6ad055a74cccd86b0db11
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2014-09-12 17:59:21 +0600

    ...

--- lib/string/replace_invalid_byte_seqs.rb
+++ lib/string/replace_invalid_byte_seqs.rb
@@ -2,13 +2,15 @@
 class String
   
   def replace_invalid_byte_seqs(replacement = "_")
-    result = ""
-    self.chars.each do |c|
-      if c.valid_encoding? then result << c
-      else result << replacement
+    if not self.valid_encoding?
+      result = ""
+      self.chars.each do |c|
+        if c.valid_encoding? then result << c
+        else result << replacement
+        end
       end
+      return result
     end
-    return result
   end
   
 end
