0c1e8509066cf8a366667197cc865990faf05f99
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2014-10-31 20:31:55 +0500

    ...

--- lib/fixed_size_queue.rb
+++ lib/fixed_size_queue.rb
@@ -1,17 +1,25 @@
 
 class FixedSizeQueue
   
-  def initialize(size)
-    @content = [nil] * size
-    @end = @begin = 0
+  def initialize(max_size)
+    # TODO
   end
   
   def empty?
-    @end == @begin
+    size == 0
   end
   
+  # Is #size() = #max_size()?
   def full?
-    
+    size == max_size
   end
+  
+  # Number of items currently stored in this FixedSizeQueue.
+  def size
+    # TODO
+  end
+  
+  # Maximum of #size.
+  attr_reader :max_size
   
 end
