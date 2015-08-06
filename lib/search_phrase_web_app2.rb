5eaef48344df7d6deac0c6e0681b768c12f0207a
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2015-08-06 18:37:34 +0500

    ...

--- lib/search_phrase_web_app2.rb
+++ lib/search_phrase_web_app2.rb
@@ -1,1 +1,36 @@
+# encoding: UTF-8
+require 'sinatra/base'
+require 'expiring_hash_map'
 
+
+class SearchPhraseWebApp < Sinatra::Application
+  
+  template :layout do
+    <<-ERB
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>Search Phrase</title>
+        </head>
+        <body>
+          <%= yield %>
+        </body>
+      </html>
+    ERB
+  end
+  
+#          <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+#           <hr style="height: 1px"/>
+#           <center><small><a href="<%=@source_code_url%>">Source code</a> | <a href="mailto:<%=@email%>">Contact me</a></small></center>
+  
+  template :index do
+    <<-ERB
+      
+    ERB
+  end
+  
+  get '/' do
+    
+  end
+  
+end
