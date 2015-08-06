d4f98ab22a16a56fd79b6273238a2ae66c35f1e7
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2015-08-07 00:13:02 +0500

    ...

--- lib/search_phrase_web_app.rb
+++ lib/search_phrase_web_app.rb
@@ -14,6 +14,19 @@ class SearchPhraseWebApp < Sinatra::Application
   set :views, "#{APP_DIR}/views"
   set :public_folder, "#{APP_DIR}/static"
   
+  # 
+  # +search+ is a Proc which is passed with a query and a Watir::Browser, sends
+  # the query to the search engine and returns RandomAccessible collection of
+  # URLs.
+  # 
+  # +new_search_browser+ is a Proc returning a new Watir::Browser for passing
+  # it to +search+.
+  # 
+  # +cache_lifetime+ is 
+  # 
+  def initialize(search, new_search_browser, cache_lifetime = 30*60)
+  end
+  
   get "/" do
     redirect "index.html"
   end
