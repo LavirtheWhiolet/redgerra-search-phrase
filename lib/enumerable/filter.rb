b2acba0d2c1adcd6d47e4b22b7272aa11ba307b7
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2015-08-10 23:59:25 +0500

    Add Enumerable#filter().

---
+++ lib/enumerable/filter.rb
@@ -0,0 +1,27 @@
+
+module Enumerable
+  
+  # 
+  # :call-seq:
+  #   filter() { |item| ... → Array } → Enumerable
+  # 
+  # It passes +f+ with each item from this Enumerable, receives Array-s from +f+
+  # and returns their concatenation (as Enumerable).
+  # 
+  # Examples:
+  # 
+  #   ["a", "b", "c"].filter { |l| [l, l+l, l+l+l] }.to_a
+  #     #=> ["a", "aa", "aaa", "b", "bb", "bbb", "c", "cc", "ccc"]
+  #   
+  #   [1, 2, 3, 4].filter { |x| if x.odd? then [x] else [] end }.to_a
+  #     #=> [1, 3]
+  # 
+  def filter(&f)
+    r = []
+    for item in self
+      r.concat(f.(item))
+    end
+    return r
+  end
+  
+end
