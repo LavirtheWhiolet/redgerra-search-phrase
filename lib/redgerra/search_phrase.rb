fef265cbb46f103165b1a35f6fdbb5d69a4c1733
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2015-08-11 22:44:16 +0500

    ...

--- lib/redgerra/search_phrase.rb
+++ lib/redgerra/search_phrase.rb
@@ -119,8 +119,9 @@ module Redgerra
     return text_blocks
   end
 
+  # converts all consecutive white-space characters to " ".
   def self.squeeze_whitespace(str)
-    str.gsub(/[\u0009-\u000D\u0020\u0085\u00A0\u1680\u180E\u2000-\u200A\u2028
+    str.gsub(/[\u0009-\u000D\u0020\u0085\u00A0\u1680\u180E\u2000-\u200A\u2028\u2029\u202F\u205F\u3000]+/, " ")
   end
   
   def self.upcase?(word)
