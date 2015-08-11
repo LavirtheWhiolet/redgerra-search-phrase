acda51c4cccd4d814411dea21987aff364173649
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2015-08-11 19:37:18 +0500

    ...

--- lib/enumerable/split_by.rb
+++ lib/enumerable/split_by.rb
@@ -24,9 +24,3 @@ module Enumerable
   end
   
 end
-
-p    [1, 2, 3, 4, 5, 6, 7, 8].split_by { |x| x == 3 or x == 6 }
-      #=> [[1, 2], [4, 5], [7, 8]]
-  
-p    [1, 2, 3, 4].split_by { |x| x == 2 or x == 3 }
-      #=> [[1], [], [4]]
