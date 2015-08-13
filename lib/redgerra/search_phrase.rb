97ea8470a1a3b035c167ac65664259acd315aed2
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2015-08-13 21:33:05 +0500

    ...

--- lib/redgerra/search_phrase.rb
+++ lib/redgerra/search_phrase.rb
@@ -36,7 +36,7 @@ module Redgerra
     m = Memory.new
     # 
     phrases = 
-      web_search.(%("#{sloch}"), browser).
+      web_search.(%("#{sloch}"), "en", browser).
       lazy_cached_filter do |web_search_result|
         [web_search_result.page_excerpt]
       end.
