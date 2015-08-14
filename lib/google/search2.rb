b73adc5580577dd8a016ca46a48adf53b3de1be1
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2015-08-14 16:49:23 +0500

    ...

--- lib/google/search2.rb
+++ lib/google/search2.rb
@@ -118,7 +118,7 @@ module Google
         if e.response_code == "503" and (captcha_form = e.page.form(action: "CaptchaRedirect")).not_nil?
           raise ServerAsksCaptcha.new(
             "Google thinks you are bot and asks to solve a captcha",
-            "#{e.page.uri.scheme}://#{e.page.uri.host}#{e.page.root.xpath("//img/@src").first.value}",
+            "https://google.com#{e.page.root.xpath("//img/@src").first.value}",
             &lambda do |captcha_answer|
               captcha_form.field(name: "captcha").value = captcha_answer
               handling_browser_exceptions do
