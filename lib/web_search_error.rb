a2e82631c58f8ef0f664c9a2fc7ed08ed25e735c
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2015-08-28 21:17:47 +0500

    ...

--- lib/web_search_error.rb
+++ lib/web_search_error.rb
@@ -1,11 +1,20 @@
 
 # Error occured during Web-search.
 class WebSearchError < Exception
-
-  def initialize(user_readable_message)
+  
+  # 
+  # +user_readable_message+ is the value for #user_readable_message.
+  # 
+  # +cause+ is the value for #cause.
+  # 
+  def initialize(user_readable_message, cause = nil)
     super(user_readable_message)
+    @cause = cause
   end
   
   alias user_readable_message message
+  
+  # Exception caused this WebSearchError. It may be nil.
+  attr_reader :cause
   
 end
