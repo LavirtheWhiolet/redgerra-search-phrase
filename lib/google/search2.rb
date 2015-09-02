133ff9f03a94c8e9fd421c92aa1bf5de64ade0ba
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2015-09-02 14:59:45 +0500

    ...

--- lib/google/search2.rb
+++ lib/google/search2.rb
@@ -112,7 +112,10 @@ module Google
     end
     
     def next_page_url_from(page, page_uri)
-      a = page.xpath("//a").find { |a| text_from(a).strip == "Next" }
+      a = page.xpath("//a").find do |a|
+        a["href"].start_with? "/search?" and
+        text_from(a).strip == "Next"
+      end
       return nil unless a
       href = a["href"]
       url = "#{page_uri.scheme}://#{page_uri.host}#{href}"
