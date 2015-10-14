560c8ee995723e704feb964d3b9c6ec230b30795
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2015-10-14 20:34:34 +0500

    ...

--- lib/redgerra/sloch.rb
+++ lib/redgerra/sloch.rb
@@ -1,26 +1,58 @@
-require 'redgerra/text'
+require 'redgerra/string/parse'
+require 'string/hex_encode'
 
 module Reggerra
   
-  # Redgerra::Sloch is a regular expression with non-standard syntax:
-  # - #escape(str) matches +str+
-  # - 
+  # Sloch is a regular expression with non-standard syntax:
   # 
+  # - Sloch::escape(str) matches +str+.
+  # - Sloch::WORD matches any word.
+  # - Sloch::ASTERISK matches 1 or 2 words, any number of " " and one optional
+  #   "," between them.
+  # - All combinators from Regexp ("()", "{}", "x*", "x+", "x?") have the same
+  #   meaning as in Regexp.
+  # 
+  # Example:
+  #   
+  #   Sloch.new(
+  #     "#{Sloch::escape("do", false)} #{Sloch::ASTERISK} #{Sloch::escape("flop", false)}"
+  #   )
   # 
   class Sloch
     
+    # Private for Sloch.
+    # 
+    # Macro. See source code.
+    # 
+    def self.oo(str)
+      "O#{str.hex_encode}O"
+    end
+    
     def self.escape(str, case_sensitive = true)
-      Text.encode(str)
+      parse do |token, type|
+        case type
+        when :word
+          if case_sensitive then
+            raise "not implemented"
+          else
+            "W[01]#{token.downcase.hex_encode}Y\\h+W"
+          end
+        when :other
+          oo(token)
+        end
+      end
     end
     
-    def self.word
+    WORD = "W[01]\\h+Y\\h+W"
+    
+    ASTERISK = "#{WORD}((#{oo ' '})?(#{oo ','})?(#{oo ' '})?#{WORD})?"
+    
+    def initialize(str)
+      @encoded_regexp = Regexp.new(str)
     end
     
-    private
-    
-    def initialize(regexp)  # :not-new:
-      @regexp = regexp
-    end
+    # Accessible to Text only.
+    attr_reader :encoded_regexp
     
   end
   
